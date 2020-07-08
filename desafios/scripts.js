9194a98a55de47d0ba934b21a5ece12262c73f6d
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2020-07-08 17:21:15 -0300

    desafio 2-3

---
+++ desafios/scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
+
+for(let card of cards) {
+    const modalId = card.getAttribute("id")
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add("active")
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${modalId}`
+    })
+}
+
+document.querySelector('.maximize-modal').addEventListener("click", function(){
+    modal.classList.add("active")
+})
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+})
