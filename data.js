266b68aa77c4fbc07123af364259896780689058
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2020-07-30 16:36:09 -0300

    dados dinâmicos
    informações de cursos e descrição de forma dinâmica

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos",
+        content: "5 <strong>módulos</strong>",
+        price: "Gratuito"
+    },
+    {
+        id: "launchbase",
+        title: "<strong>Domine a programação do zero</strong> e tenha acesso às melhores oportunidades do mercado",
+        content: "15 <strong>módulos</strong>",
+        price: "Pago"
+    },
+    {
+        id: "gostack",
+        title: "<strong>Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile",
+        content: "30 <strong>módulos</strong>",
+        price: "Pago"
+    }
+]
--- server.js
+++ server.js
@@ -2,21 +2,40 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const cards = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render("courses")
+    return res.render("courses", { items: cards })
 })
 
 server.get("/about", function(req, res) {
-    return res.render("about")
+    about = {
+        img_url: "https://pbs.twimg.com/profile_images/1271517147349626881/Mf1UjRa0_400x400.jpg",
+        name: "Rocketseat",
+        content: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        list: [
+            {name: "JavaScript"},
+            {name: "| ReactJS"},
+            {name: "| ReactNative"},
+            {name: "| NodeJS"}
+        ],
+        links: [
+            {name: "GitHub" , url: "https://github.com/Rocketseat"},
+            {name: "Instagram" , url: "https://www.instagram.com/rocketseat_oficial/"},
+            {name: "Facebook" , url: "https://www.facebook.com/rocketseat"},
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.use(function(req, res) {
